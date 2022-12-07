360dddf57fb73c5c49c80a927c748ab6127e76d2
Tatiana Liabina <t.liabina@icloud.com>
2022-12-07 21:07:49 +0400

    add task

--- Home_work_8.3/Program.cs
+++ Home_work_8.3/Program.cs
@@ -8,66 +8,73 @@
 15 18*/
 
 
-
-
-int [,] GetArray(int rows, int colomns)
+int[,] GetArray(int rows, int colomns)
 {
     int[,] array = new int[rows, colomns];
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < array.GetLength(-1); j++)
+        for (int j = 0; j < colomns; j++)
         {
             array[i, j] = new Random().Next(0, 10);
         }
     }
     return array;
 }
+
 void PrintArray(int[,] array)
 {
+    int rows = array.GetLength(0);
+    int colomns = array.GetLength(1);
 
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < array.GetLength(-1); j++)
+        for (int j = 0; j < colomns; j++)
         {
             Console.Write(array[i, j] + " ");
         }
 
         Console.WriteLine();
     }
-
 }
 
-void PowerArray(int[,] array, int[,] matrix)
+int[,] PowerArray(int[,] array1, int[,] array2)
 {
+    int rows = array1.GetLength(0);
+    int colomns = array1.GetLength(1);
 
+    int[,] result = new int[rows, colomns];
 
-    int[,] arrays = new int[rows, colomns];
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < colomns; j++)
         {
-                    int res = 0;
-                    for(int k = 0; k<array.GetLength(0);k++)
-                    {
-                        res += array(i,k)*matrix(k,j);
-                    }
-                    
-                Console.WriteLine(array[rows, colomns] + " ");
+            int res = 0;
 
+            for (int k = 0; k < rows; k++)
+            {
+                res += array1[i, k] * array2[k, j];
             }
+
+            result[i, j] = res;
         }
     }
-    return arrays;
+
+    return result;
 }
 
-Console.WriteLine("Задайте размер массивов: ");
+Console.Write("Задайте размер массивов: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int array = GetArray(n,n);
-int matrix = GetArray(n,n);
+var array1 = GetArray(n, n);
+var array2 = GetArray(n, n);
 
-PrintArray(GetArray(array));
+//int[,] array1 = { { 2, 4 }, { 3, 2 } };
+//int[,] array2 = { { 3, 4 }, { 3, 3 } };
+
+PrintArray(array1);
 Console.WriteLine("==============");
-PrintArray(GetArray(matrix));
+PrintArray(array2);
 Console.WriteLine("==============");
-PowerArray(array, matrix);
+
+var matrix = PowerArray(array1, array2);
+PrintArray(matrix);
